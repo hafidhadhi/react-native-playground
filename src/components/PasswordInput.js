bf14958d0b5d2b782bd860966da71a785f493a53
hafidhadhi <hafidh.adhi@gmail.com>
2020-12-03 11:23:06 +0700

    adding icon in text input

--- src/components/PasswordInput.js
+++ src/components/PasswordInput.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
     StyleSheet,
     View,
     TextInput
-} from 'react-native';
+} from 'react-native'
+import FeatherIcon from 'react-native-vector-icons/Feather'
 import PropTypes from 'prop-types'
 
 export default class PasswordInput extends Component {
@@ -15,6 +16,7 @@ export default class PasswordInput extends Component {
         return (
             <View style={styles(this.props).container}>
                 <TextInput style={styles(this.props).textInput} placeholder="Password" onChangeText={(text) => this.props.onChangeText(text)} />
+                <FeatherIcon name="lock" size={30} color="#C0C0C0" style={styles(this.props).icon} />
             </View>
         )
     }
@@ -23,9 +25,16 @@ export default class PasswordInput extends Component {
 const styles = (props) => StyleSheet.create({
     container: {
         marginTop: 16,
-        flexDirection: "row"
+        flexDirection: "row",   
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: "#C0C0C0",
+        backgroundColor: "#FCFCFC"
     },
     textInput: {
         flex: 1
+    },
+    icon: {
+        margin: 8
     }
 })
--- src/components/UsernameInput.js
+++ src/components/UsernameInput.js
@@ -5,16 +5,26 @@ import {
     TextInput
 } from 'react-native';
 import PropTypes from 'prop-types'
+import FeatherIcon from 'react-native-vector-icons/Feather'
 
 export default class UsernameInput extends Component {
+
     static propTypes = {
         onChangeText: PropTypes.func
+    }
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            text: ""
+        }
     }
 
     render() {
         return (
             <View style={styles(this.props).container}>
                 <TextInput style={styles(this.props).textInput} placeholder="Username/email" onChangeText={(text) => this.props.onChangeText(text)} />
+                <FeatherIcon name="mail" size={30} color="#C0C0C0" style={styles(this.props).icon} />
             </View>
         )
     }
@@ -23,9 +33,16 @@ export default class UsernameInput extends Component {
 const styles = (props) => StyleSheet.create({
     container: {
         marginTop: 16,
-        flexDirection: "row"
+        flexDirection: "row",
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: "#C0C0C0",
+        backgroundColor: "#FCFCFC"
     },
     textInput: {
         flex: 1
+    },
+    icon: {
+        margin: 8
     }
 })
